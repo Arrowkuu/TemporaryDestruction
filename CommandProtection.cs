9415f5b00a09ce19d6cf51b69230e610cb1d60cc
Arrowkuu <arroweeek@gmail.com>
2017-06-26 11:49:52 +0200

    Update #3
    Fix System.Threading

--- CommandProtection.cs
+++ CommandProtection.cs
@@ -1,10 +1,6 @@
 ﻿using Rocket.API;
 using Rocket.Unturned.Chat;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace arrowkuu.temporarydestruction
